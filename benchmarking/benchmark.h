c9ef4c60e2d349faf9a2208a3d9f86069a07ef14
Karácsony Máté <k_mate@inf.elte.hu>
2016-04-27 18:51:55 +0200

    Measuring in microseconds instead of milliseconds

--- benchmarking/benchmark.h
+++ benchmarking/benchmark.h
@@ -13,5 +13,5 @@ unsigned long long nsecs = 0;
     nsecs += (end.tv_sec - start.tv_sec) * 1e9 + (end.tv_nsec - start.tv_nsec);
 
 #define print_time() \
-    fprintf(stderr, "%llu\n", nsecs / (unsigned long long)1e6);
+    fprintf(stderr, "%llu us\n", nsecs / (unsigned long long)1e3);
 
